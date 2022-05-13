d11e4391513c3ab65e1791de26ee840db9edf705
Yancho Georgiev <yanchogeorgiev@gmail.com>
2022-05-13 14:13:53 +0300

    protobuf struct wrapper

--- src/index.ts
+++ src/index.ts
@@ -14,5 +14,36 @@ export const loadSync = (filename: string | string[], options?: Options): Packag
     },
   } as any;
 
+  wrappers['.google.protobuf.Struct'] = {
+    /**
+     * Encodes a JSON object into a protobuf {@link Struct}.
+     *
+     * @param {Object.<string, *>} value the JSON object.
+     * @returns {Struct}
+     */
+     fromObject(json: {[key: string]: any}): any {
+      const fields = {};
+      Object.keys(json).forEach(key => {
+        // If value is undefined, do not encode it.
+        if (typeof json[key] === 'undefined') return;
+        fields[key] = this.fromObject(json[key]);
+      });
+      return {fields};
+    },
+    /**
+     * Decodes a protobuf {@link Struct} into a JSON object.
+     *
+     * @param {Struct} struct the protobuf struct.
+     * @returns {Object.<string, *>}
+     */
+     toObject({fields = {}}: any): {[key: string]: any} {
+      const json = {};
+      Object.keys(fields).forEach(key => {
+        json[key] = this.toObject(fields[key]);
+      });
+      return json;
+    }
+  };
+
   return _loadSync(filename, options);
 };
